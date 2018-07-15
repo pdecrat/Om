5bb376d3cd0a0138e9893c2cda3a5c12c2c2d19b
Philippe Decrat <philippe.decrat@gmail.com>
2018-07-15 10:35:58 +0200

    hey

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import { Provider } from 'react-redux';
+// import thunk from 'redux-thunk';
+// import { createStore, applyMiddleware } from 'redux';
+
+import './GlobalStyle';
+import Login from './Login';
+import Content from './Content/Content';
+import Navigation from './Navigation/Navigation';
+
 
 const App = () =>
-  <div className="container">
-    <header>
-      <h1>Hello World !</h1>
-    </header>
-  </div>
+  <Router>
+    <div>
+      <Navigation />
+      <Content />
+    </div>
+  </Router>
 
 export default App
