b4c62605887ea3b3fcf0f6429c2c2787ce4a7de4
Philippe Decrat <philippe.decrat@gmail.com>
2018-08-09 07:59:24 +0200

    remove actions on server start

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -1,10 +1,10 @@
 import { Meteor } from 'meteor/meteor';
 
 import Spaces from '/imports/api/Spaces/Spaces';
+import Actions from '/imports/api/Actions';
 
 Meteor.startup(() => {
-  Meteor.users.remove({});
-  Spaces.remove({});
+  Actions.remove({})
   if (Spaces.find().count() === 0) {
     Spaces.insert({
       name: 'om',
